f76d6af9bae8f25f6e20376780d8bd65dce4ac55
salman-itShaala <salman.shaikh@itshaala.com>
2025-02-25 15:57:06 +0530

    added code from operators session

---
+++ day -11/main.js
@@ -0,0 +1,57 @@
+// Increment(++) and Decrement(--) operators
+
+let num = 10;
+
+// num = num + 10;
+
+console.log(num--); // 10
+
+console.log(num); // 9
+
+console.log(++num); // 10
+
+console.log(num); // 10
+
+// Compund assignment operators
+
+let sum = 10;
+
+sum += 10; // sum = sum + 10;
+
+console.log(sum); // 20
+
+sum -= 15; // sum = sum - 15;
+
+console.log(sum); // 5
+
+sum **= 2; // sum = sum ** 2; --> sum = 5 ** 2;
+
+console.log(sum);
+
+// Ternary operator
+
+let age = 16;
+
+age > 18 ? console.log("You can vote") : console.log("You can not vote");
+
+console.log(10 == 10);
+
+console.log("TEN" == 10);
+
+console.log("10" == 10); // true
+
+console.log("10" === 10); // false
+
+let firstName = "Salman ";
+
+let lastName = "Shaikh";
+
+let fullName = firstName + lastName;
+
+let intro = "My name is " + fullName;
+
+console.log(intro);
+
+// to convert given string into number
+console.log(+"10");
+console.log("10");
---
+++ day -11/script.js
@@ -0,0 +1,87 @@
+// Operators
+
+// 1. Arithmatic operators
+
+let num1 = 20;
+
+let num2 = 10;
+
+// i. addition +
+
+let sum = num1 + num2;
+
+console.log(sum);
+
+// ii. substaction
+
+console.log(num1 - num2);
+
+// iii. multiplication
+
+console.log(num1 * num2);
+
+// iv. division
+
+console.log(num1 / num2);
+
+// v. modulo operator --> remainder
+
+console.log(num1 % num2);
+
+// vi. exponent operator
+
+console.log(3 ** 5); // 3 ^ 5
+
+// 10 / 0 --> Infinity
+// -10 / 0 --> -Infinity
+// 0 / 0 --> NaN
+
+// Comparison operators
+
+console.log(num1 < num2); // false
+
+console.log(num1 > num2); // true
+
+console.log(10 <= 10); // true
+
+console.log(10 >= 10); // true
+
+// Equality check operators
+
+console.log(2 == 2); // true
+
+console.log(10 == 1); // false
+
+console.log(2 != 2); // false
+
+console.log(10 != 1); // true
+
+// Logical operators
+
+// AND
+
+console.log(true && true); // true
+
+console.log(true && false);
+
+console.log(false && false);
+
+console.log(false && true);
+
+// OR
+
+console.log(false || false); // false
+
+console.log(false || true);
+
+console.log(true || true);
+
+console.log(true || false);
+
+// NOT
+
+console.log(!true); // false
+
+console.log(!false); // true
+
+console.log(~3);
