e8aef4aa81633f76fea1ef61017c5c9d528865c7
salman-itShaala <salman.shaikh@itshaala.com>
2025-02-26 15:56:57 +0530

    added notes and code

---
+++ day -12/main.js
@@ -0,0 +1,65 @@
+let age = 35;
+
+if (age > 18) {
+  console.log("You can vote");
+  console.log("hii");
+} else {
+  console.log("You can not vote");
+  console.log("Hello");
+}
+
+// check if given number is odd or even
+
+let num = 10;
+
+if (num % 2 === 0) {
+  console.log("even");
+} else {
+  console.log("Odd");
+}
+
+// check whether given number is positive or negative
+
+let num2 = 0;
+
+if (num2 > 0) {
+  console.log("Given number is positive");
+} else if (num2 < 0) {
+  console.log("Given number is negative");
+} else {
+  console.log("Given number is zero");
+}
+
+// age > 18 ? console.log("You can vote") : console.log("You can not vote");
+
+// Decision making statements
+
+// switch (weekdays) {
+//   case 1:
+//     console.log("Monday");
+
+//     break;
+
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+//   case 4:
+//     console.log("Thursday");
+//     break;
+//   case 5:
+//     console.log("Friday");
+//     break;
+//   case 6:
+//     console.log("Saturday");
+//     break;
+//   case 7:
+//     console.log("Sunday");
+//     break;
+//   default:
+//     console.log("Invalid Input");
+
+//     break;
+// }
