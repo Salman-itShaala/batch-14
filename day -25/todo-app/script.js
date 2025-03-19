f76b11bbb45a8a91636925f498239fc70cea3bf5
salman-itShaala <salman.shaikh@itshaala.com>
2025-03-19 14:19:30 +0530

    added localstorage support in todos app

--- day -25/todo-app/script.js
+++ day -25/todo-app/script.js
@@ -1,6 +1,10 @@
 const todoInput = document.getElementById("todo-input");
 const todosParent = document.getElementById("todos-wrapper");
 
+const todos = JSON.parse(localStorage.getItem("todosArr")) || []; // ["do something"]
+
+displayTodos();
+
 function addTodo() {
   const todoValue = todoInput.value;
 
@@ -10,13 +14,25 @@ function addTodo() {
     return;
   }
 
-  // creating para element
-  const todoPara = document.createElement("p");
+  todos.push(todoValue);
+  localStorage.setItem("todosArr", JSON.stringify(todos));
 
-  todoPara.innerText = todoValue;
-
-  // adding element to front end
-  todosParent.appendChild(todoPara);
+  // magic box --> todos --> front end
+  displayTodos();
 
   todoInput.value = "";
+}
+
+function displayTodos() {
+  todosParent.innerHTML = "";
+
+  for (let i = 0; i < todos.length; i++) {
+    // creating para element
+    const todoPara = document.createElement("p");
+
+    todoPara.innerText = todos[i];
+
+    // adding element to front end
+    todosParent.appendChild(todoPara);
+  }
 }
