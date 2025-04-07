97a41ed70be167d913b0636ddad71a30c9a7b243
salman-itShaala <salman.shaikh@itshaala.com>
2025-04-07 15:13:10 +0530

    added css grid code

---
+++ day -36/script.js
@@ -0,0 +1,26 @@
+// 1. function declaration
+
+// function doSum(num1, num2) {
+//   const sum = num1 + num2;
+//   return sum;
+// }
+
+// console.log(doSum(20, 30));
+
+// 2. Function expression
+
+// const doSum = function (num1, num2) {
+//   const sum = num1 + num2;
+//   return sum;
+// };
+
+// console.log(doSum(20, 30));
+
+// 3. Arrow function
+
+const doSum = (num1, num2) => {
+  const sum = num1 + num2;
+  return sum;
+};
+
+console.log(doSum(21, 30));
