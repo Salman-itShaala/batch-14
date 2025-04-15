c851730701a67321fad3c81905a792a918ac2257
salman-itShaala <salman.shaikh@itshaala.com>
2025-04-15 16:02:40 +0530

    added async joke telling code

---
+++ day -41/joke-tellling.js
@@ -0,0 +1,15 @@
+// https://official-joke-api.appspot.com/random_joke
+const setupPara = document.getElementById("setup");
+const punchlinePara = document.getElementById("punchline");
+
+function getJoke() {
+  const resPromise = fetch("https://official-joke-api.appspot.com/random_joke");
+
+  resPromise
+    .then((data) => data.json())
+    .then((joke) => {
+      setupPara.innerText = joke.setup;
+      punchlinePara.innerText = joke.punchline;
+    })
+    .catch((err) => console.log(err));
+}
